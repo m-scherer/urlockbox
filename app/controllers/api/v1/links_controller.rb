2f613b8d816a775e15c2083ab1abfcff5c56c0e3
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-13 17:28:09 -0700

    Change to push read links only

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::LinksController < ApplicationController
     @link = Link.find(params[:id])
     if @link.update_attributes(link_params)
       render json: @link
+      publish_user(current_user)
+      publish_link(@link)
     else
       render json: @link.errors.full_messages, status: 500
     end
@@ -14,4 +16,16 @@ class Api::V1::LinksController < ApplicationController
   def link_params
     params.permit(:read)
   end
+
+
+    def publish_link(link)
+      pub = Publisher.new
+      pub.publish_link_to_queue(link)
+    end
+
+    def publish_user(user)
+      pub = Publisher.new
+      pub.publish_user_to_queue(user)
+    end
+
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -14,29 +14,15 @@ class LinksController < ApplicationController
     elsif link.save
       flash[:success] = "Successfully created #{link.title}"
       redirect_to links_path
-      publish_user(current_user)
-      publish_link(link)
     else
       render :index
     end
   end
 
-  
-
   private
 
   def link_params
     params.require(:link).permit(:title, :url)
-  end
-
-  def publish_link(link)
-    pub = Publisher.new
-    pub.publish_link_to_queue(link)
-  end
-
-  def publish_user(user)
-    pub = Publisher.new
-    pub.publish_user_to_queue(user)
   end
 
 end
